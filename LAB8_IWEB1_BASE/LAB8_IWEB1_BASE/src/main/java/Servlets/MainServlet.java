a245d00fe985190a5f5c54d3e6a3e017d83de189
Jun23333333 <106772376+Jun23333333@users.noreply.github.com>
2022-06-13 20:31:58 -0500

    Update MainServlet.java

--- LAB8_IWEB1_BASE/LAB8_IWEB1_BASE/src/main/java/Servlets/MainServlet.java
+++ LAB8_IWEB1_BASE/LAB8_IWEB1_BASE/src/main/java/Servlets/MainServlet.java
@@ -1,4 +1,5 @@
 package Servlets;
+import Beans.Banda;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,15 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "MainServlet",urlPatterns = {""})
+@WebServlet(name = "MainServlet",urlPatterns = {"/alexpipipipipi"})
 public class MainServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        RequestDispatcher view =request.getRequestDispatcher("index.jsp");
+        RequestDispatcher view =request.getRequestDispatcher("lista_recomendados.jsp");
         view.forward(request,response);
     }
 }
