945775f3bb836183fb083fbb73db6a70294b5f97
miguelac1112 <106561075+miguelac1112@users.noreply.github.com>
2022-06-13 20:51:15 -0500

    Add files via upload

---
+++ LAB8_IWEB1_BASE/LAB8_IWEB1_BASE/src/main/java/Servlets/CancionServlet.java
@@ -0,0 +1,53 @@
+package Servlets;
+
+import Daos.CancionDao;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "CancionServlet", value = "/listaCanciones")
+public class CancionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String action = request.getParameter("a") == null? "inicio" : request.getParameter("a");
+        CancionDao cancionDao = new CancionDao();
+        switch (action) {
+            case "inicio" -> {
+                request.setAttribute("inicio","vacio");
+
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("listaCanciones.jsp");
+                requestDispatcher.forward(request,response);
+            }
+            case "listar" -> {
+                request.setAttribute("listaCanciones", cancionDao.obtenerTodasCanciones());
+
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("listaCanciones.jsp");
+                requestDispatcher.forward(request, response);
+            }
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String action = request.getParameter("a") == null ? "listar" : request.getParameter("a");
+        CancionDao cancionDao = new CancionDao();
+
+        switch (action){
+            case "buscar" -> {
+                String textoBuscar = request.getParameter("textoBuscar");
+                request.setAttribute("textoBuscar",textoBuscar);
+                request.setAttribute("listaCanciones", cancionDao.CancionesPorBanda(textoBuscar));
+
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("jobs/lista.jsp");
+                requestDispatcher.forward(request, response);
+            }
+        }
+    }
+}
