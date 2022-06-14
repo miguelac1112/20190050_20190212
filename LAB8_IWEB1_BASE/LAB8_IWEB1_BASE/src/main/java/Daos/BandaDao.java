be614d08a433612c8c0ae77cd43815807526c41f
Jun23333333 <106772376+Jun23333333@users.noreply.github.com>
2022-06-13 19:26:08 -0500

    banda dao actualizado

--- LAB8_IWEB1_BASE/LAB8_IWEB1_BASE/src/main/java/Daos/BandaDao.java
+++ LAB8_IWEB1_BASE/LAB8_IWEB1_BASE/src/main/java/Daos/BandaDao.java
@@ -18,8 +18,24 @@ public class BandaDao {
     }
 
     //En este caso se usa preparedStatement
+
+
     public ArrayList<Banda> obtenerListaBandas(String filter) {
         ArrayList<Banda> listabandas = new ArrayList<>();
+
+        String sql = "select idcancion, nombre_cancion, banda from cancion c, reproduccion r where c.idcancion=r.cancion_idcancion group by cancion_idcancion having count(*) >2 order by count(*) desc";
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery("select * from jobs");) {
+
+            while (rs.next()) {
+                Banda banda = new Banda();
+                banda.Banda(rs.getInt(1),rs.getString(2),rs.getString(3));
+                listabandas.add(banda);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         return listabandas;
     }
