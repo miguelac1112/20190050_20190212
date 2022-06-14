7872cd3cb4cc8e5a28f62371de955f86ed6fbdfa
Jun23333333 <106772376+Jun23333333@users.noreply.github.com>
2022-06-13 20:32:58 -0500

    Update BandaDao.java

--- LAB8_IWEB1_BASE/LAB8_IWEB1_BASE/src/main/java/Daos/BandaDao.java
+++ LAB8_IWEB1_BASE/LAB8_IWEB1_BASE/src/main/java/Daos/BandaDao.java
@@ -20,13 +20,13 @@ public class BandaDao {
     //En este caso se usa preparedStatement
 
 
-    public ArrayList<Banda> obtenerListaBandas(String filter) {
+    public ArrayList<Banda> obtenerListaBandas(){
         ArrayList<Banda> listabandas = new ArrayList<>();
 
         String sql = "select idcancion, nombre_cancion, banda from cancion c, reproduccion r where c.idcancion=r.cancion_idcancion group by cancion_idcancion having count(*) >2 order by count(*) desc";
         try (Connection connection = DriverManager.getConnection(url, user, pass);
              Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery("select * from jobs");) {
+             ResultSet rs = stmt.executeQuery(sql);) {
 
             while (rs.next()) {
                 Banda banda = new Banda();
@@ -38,6 +38,83 @@ public class BandaDao {
         }
 
         return listabandas;
+    }
+
+    public ArrayList<Banda> obtenerListaFavorito(){
+        ArrayList<Banda> listabandas = new ArrayList<>();
+
+        String sql = "select idcancion, nombre_cancion, banda, favorito from cancion c, reproduccion r where c.idcancion=r.cancion_idcancion";
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql);) {
+
+            while (rs.next()) {
+                Banda banda = new Banda();
+                banda.Banda(rs.getInt(1),rs.getString(2),rs.getString(3));
+                banda.setFavorito(rs.getInt(4))
+                listabandas.add(banda);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return listabandas;
+    }
+
+    public void actualizar(String id) {
+
+        String user = "root";
+        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/lab6sw1";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sql = "UPDATE cancion SET favorito =? where idcancion = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+
+            pstmt.setInt(1, 1);
+            pstmt.setInt(2, id);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void no_favorito(String id) {
+
+        String user = "root";
+        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/lab6sw1";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sql = "UPDATE cancion SET favorito =? where idcancion = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+
+            pstmt.setInt(1, 0);
+            pstmt.setInt(2, id);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
